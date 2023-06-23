e66004dd36c71f268606716e4017b4d9c1ccc960
luckyfullstack2023 <luckyfullstack2023@gmail.com>
2023-06-23 12:34:43 +0530

    L-52 from batch-2 assignment done sir

---
+++ js/script.js
@@ -0,0 +1,29 @@
+
+
+// single-line comment
+
+/* 
+Multi Line comment
+
+Lets take a JS object for storing student information
+*/
+
+var student1_name = "Lucky";
+
+var student1_surname = "Khamari";
+
+var student1 = {
+
+    // 1.property:value
+    name:" Lucky",
+    surname:'Khamari',
+
+    // 2.
+};//JS
+
+// How many way i can display the output of a JS code
+
+// object.perpertyname  (formula)
+// 1. first way to see the js output
+
+console.log("Hello" + student1.name);//+ is a concatination operator
---
+++ js/script2.js
@@ -0,0 +1,21 @@
+// var 1995 - 2022
+
+// Let 2015 ES6
+
+// Const 2016 ES6
+
+const student4 = {
+    // 1. Property:Value
+    name:"lucky_4",
+    surname:'Khamri_4',
+    father_name:`G.C.Khamari`,
+
+    // 2.
+}; //JS object
+
+// How many way i see the JS output
+
+// 4th_way document.write();
+
+// object.propertyname
+document.write('<div>'+student4.name+'</div>');
